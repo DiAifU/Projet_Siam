e4ace6fef18d533164df6bde3cfd9b1d05d4aaf5
kotres <mkuhlburger@gmail.com>
2015-11-29 20:07:41 +0100

    modifié plateau etre integre et plateau exister piece

--- src/plateau_siam.c
+++ src/plateau_siam.c
@@ -44,14 +44,44 @@ void plateau_initialiser(plateau_siam* plateau)
 
 int plateau_etre_integre(const plateau_siam* plateau)
 {
-    return 1; //coder cette fonction
+  assert(plateau!=NULL);
+  //on verifie que le nombre de rochers est ok
+  int nbrRochers=  plateau_denombrer_type(plateau,rocher);
+  if(nbrRochers>NBR_ROCHERS)
+    return 0;
+  //on verifie que le nombre d'elephants est ok
+  int nbrElephants=plateau_denombrer_type(plateau,elephant);
+  if (nbrElephants>NBR_ANIMAUX)
+    return 0;
+  //on verifie que le nombre de rhinoceros est ok
+  int nbrRhinoceros=plateau_denombrer_type(plateau,rhinoceros);
+  if(nbrRhinoceros>NBR_ANIMAUX)
+    return 0;
+  /*on vérifie que les pieces sont integres
+  le code est celui de l'énoncé 2 pour denombrer type
+  que j'ai adapte pour voir si chaque case du plateau
+  est integre*/
+  int kx=0;
+  for (kx=0; kx < NBR_CASES; ++kx)
+    {
+      int ky=0;
+      for (ky=0; ky < NBR_CASES; ++ky)
+	{
+	  const piece_siam* piece=
+	    plateau_obtenir_piece_info(plateau,kx,ky);
+	  assert(piece!=NULL);
+	  if(piece_etre_integre(piece)==0)
+	    return 0;// on renvoie 0 si on trouve qu'une piece n'est pas integre
+	}
+    }
+  //si tout va bien on renvoie 1
+  return 1;
 }
 
 piece_siam* plateau_obtenir_piece(plateau_siam* plateau,int x,int y)
 {
   assert(coordonnees_etre_dans_plateau(x,y));
   assert(plateau!=NULL);
-  assert(plateau_etre_integre(plateau));
   return &(plateau->piece[x][y]);
 }
 
@@ -59,7 +89,6 @@ const piece_siam* plateau_obtenir_piece_info(const plateau_siam* plateau,int x,i
 {
   assert(coordonnees_etre_dans_plateau(x,y));
   assert(plateau!=NULL);
-  assert(plateau_etre_integre(plateau));
   return &(plateau->piece[x][y]);
 }
 
@@ -67,24 +96,38 @@ const piece_siam* plateau_obtenir_piece_info(const plateau_siam* plateau,int x,i
 
 int plateau_denombrer_type(const plateau_siam* plateau,type_piece type)
 {
-    //fonction a modifier avec l'enonce
-    int i=0;
-    int j=0;
-    int x=0;
-    for(i=0;i<5;++i)
+  assert(plateau!=NULL);
+  int compteur=0;
+  int kx=0;
+  for (kx=0; kx < NBR_CASES; ++kx)
     {
-        for(j=0;j<5;++j)
-        {
-            if(plateau->piece[i][j].type==type)
-                x++;
-        }
+      int ky=0;
+      for (ky=0; ky < NBR_CASES; ++ky)
+	{
+	  const piece_siam* piece=
+	    plateau_obtenir_piece_info(plateau,kx,ky);
+	  assert(piece!=NULL);
+	  if(piece->type==type)
+	    compteur++;
+	}
     }
-    return x;
+  return compteur;
+  
 }
 
 int plateau_exister_piece(const plateau_siam* plateau,int x,int y)
 {
-    return 1; //coder cette fonction
+  //on vérifie que les variables donnees sont bonnes
+  assert(plateau!=NULL);
+  assert(coordonnees_etre_dans_plateau(x,y));
+  const piece_siam* piece=
+    plateau_obtenir_piece_info(plateau,x,y);// on recupere les infos de la piece
+  assert(piece!=NULL);// on verifie que le pointeur est non nul et que la piece est integre
+  assert(piece_etre_integre(piece));
+  if(piece->type==case_vide)// on regade si la case est vide et on renvoie 0 si oui
+    return 0;
+  else
+    return 1;// si elle ne l'est pas alors on renvoie 1
 }
 
 void plateau_afficher(const plateau_siam* plateau)
