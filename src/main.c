981a26b32cc0945cbe4e79812f9c01fe27023f35
diaifu <nicolas.montvernay@gmail.com>
2015-12-01 11:24:47 +0100

    Ajout code fonction plateau_modification_changer_orientation_piece

--- src/main.c
+++ src/main.c
@@ -20,20 +20,7 @@ void test_lancement()
 int main()
 {
 
-  type_piece type = elephant;
-  printf("%d", type);
-  
-  int est_ce_un_elephant = (type==elephant);
-  int est_ce_un_rocher = (type == rocher);
-  printf("%d %d", est_ce_un_elephant, est_ce_un_rocher);
-  
-  
-    //decommentez la ligne suivante pour lancer les tests
-    test_lancement();
-
-    //lancement du mode interactif
-    // mode_interactif_lancer();
-
+    mode_interactif_lancer();
     return 0;
     
 }
