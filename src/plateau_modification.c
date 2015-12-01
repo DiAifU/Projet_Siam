fee3071d324661b6693398b4eb51e552e99764d1
kotres <mkuhlburger@gmail.com>
2015-12-01 11:26:05 +0100

    modification de plateau_modification_changer_orientation_piece_etre_possible

--- src/plateau_modification.c
+++ src/plateau_modification.c
@@ -28,7 +28,27 @@ void plateau_modification_introduire_piece(plateau_siam* plateau,
 
 int plateau_modification_changer_orientation_piece_etre_possible(const plateau_siam* plateau,int x0,int y0,orientation_deplacement orientation)
 {
-    return 1;//coder cette fonction
+  assert(plateau!=NULL);
+  if(coordonnees_etre_dans_plateau(x0,y0)==0)// on vérifie si on est dans le plateau
+    return 0;
+  //on récupere les infos de la piece qu'on souhaite modifier
+  const piece_siam* piece=plateau_obtenir_piece_info(plateau,x0,y0);
+  assert(piece!=NULL);
+  /* si c'est un rocher ou une case vide on ne peur pas modifier son orientation
+     puisqu'ils peuvent seulement avoir aucune aurientation*/
+  if(piece_etre_rocher(piece)==1||piece_etre_case_vide(piece)==1)
+    return 0;
+  //on vérifie qu'on travaille bien avec une piece de type animal
+  if(piece_etre_animal(piece)==0)
+    return 0;
+  //on recupere alors son orientation
+  orientation_deplacement orientationInitiale=piece_recuperer_orientation_animal(piece);
+  assert(orientation_etre_integre(orientationInitiale));
+  //on verifie qu'elle est differente de la nouvelle orientation souhaitee
+  if(orientationInitiale==orientation)
+    return 0;
+  //si toutes les conditions ont etes verifiees alors le changement d'orientation est possible
+  return 1;
 }
 
 
