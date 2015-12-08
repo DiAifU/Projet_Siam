5ffcdd3595911d40d8774434d75b7b97139ea2b7
diaifu <nicolas.montvernay@gmail.com>
2015-12-08 10:31:17 +0100

    Ajout fonction API introduction piece + maj du script clean.sh

--- src/api_siam.c
+++ src/api_siam.c
@@ -1,13 +1,50 @@
 #include "api_siam.h"
 #include "plateau_modification.h"
+#include "joueur.h"
+#include <assert.h>
+#include <stdio.h>
 
 coup_jeu api_siam_tenter_introduire_nouvelle_piece_si_possible(jeu_siam* jeu,
                                                                int x,int y,
                                                                orientation_deplacement orientation)
 {
-    //coder cette fonction
+    assert(jeu != NULL);
+    
     coup_jeu coup;
     coup_jeu_initialiser(&coup);
+    
+    if (!coordonnees_etre_dans_plateau(x, y)) {
+      // Dans le cas ou les coord ne sont pas de le
+      // plateau, coup non valide.
+      coup.valide = 0;
+    }
+    else if (!coordonnees_etre_bordure_plateau(x, y)) {
+      // Dans le cas ou la pièce n'est pas introduite sur le bord
+      // du plateau, le coup n'est pas valide.
+      coup.valide = 0;
+    }
+    else if (!orientation_etre_integre_deplacement(orientation)) {
+      // L'orientation n'est pas integre, ou est aucune_orientation
+      // Le coup n'est donc pas valide.
+      coup.valide = 0;
+    }
+    else if (!piece_etre_case_vide(plateau_obtenir_piece_info(&jeu->plateau, x, y))) /******* TODO ******** POUR L'INSTANT, ON NE GERE PAS LA POUSSEE */
+      coup.valide = 0;
+    else {
+      // Le coup est valide !!!!! YOUPIIIII
+      piece_siam* piece = plateau_obtenir_piece(&jeu->plateau,x,y);
+      piece->type = joueur_obtenir_animal(jeu->joueur);
+      piece->orientation = orientation;
+      coup.valide = piece_etre_integre(piece);
+    }
+
+    /* ********* TODO ***********
+    ** Ajouter condition victoire
+    */
+
+    if (coup.valide)
+      jeu_changer_joueur(jeu);
+
     return coup;
 }
 
