fee87d6cefb4a669e6ea29da06c58b7d836a3d0c
kotres <mkuhlburger@gmail.com>
2015-12-13 20:25:09 +0100

    ajout de poussee_realiser

--- src/poussee.c
+++ src/poussee.c
@@ -70,4 +70,38 @@ int poussee_etre_valide(const plateau_siam* plateau,int x,int y,orientation_depl
 void poussee_realiser(plateau_siam* plateau,int x,int y,type_piece type,orientation_deplacement orientation,
 		      condition_victoire_partie conditionVictoire)
 {
+  assert(plateau!=NULL);
+  assert(plateau_etre_integre(plateau));
+  assert(coordonnees_etre_dans_plateau(x,y));
+  assert(plateau_exister_piece(plateau,x,y));
+  assert(orientation_etre_integre(orientation));
+  assert(type_etre_animal(type));
+  if(poussee_etre_valide(plateau,x,y,orientation)==1){
+    /*si la poussee est possible alors on cree un buffer ou on place la piece qui est
+      deplacee et on met la nouvelle piece a sa place*/
+    piece_siam bufferPiece1;
+    piece_siam* pieceAModifier=plateau_obtenir_piece(plateau,x,y);
+    bufferPiece1=*pieceAModifier;
+    piece_definir(pieceAModifier,type,orientation);
+    /*puis on continue le deplacement des pieces jusqu'a ce qu'on atteigne une case
+      vide ou la fin du plateau*/
+    case_suivante_de_la_poussee(&x,&y,orientation);
+    piece_siam bufferPiece2;
+    while(coordonnees_etre_dans_plateau(x,y)){
+      pieceAModifier=plateau_obtenir_piece(plateau,x,y);
+      if(piece_etre_case_vide(pieceAModifier)==1)
+	{
+	  *pieceAModifier=bufferPiece1;
+	  x=NBR_CASES+1;
+	  y=NBR_CASES+1;
+	}
+      else
+      {
+	bufferPiece2=*pieceAModifier;
+	*pieceAModifier=bufferPiece1;
+	bufferPiece1=bufferPiece2;
+	case_suivante_de_la_poussee(&x,&y,orientation);
+      }
+    }
+  }
 }
