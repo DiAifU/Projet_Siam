71fdb993fd69a16f3d8a28fad695e048ce446dbe
kotres <mkuhlburger@gmail.com>
2015-12-15 11:15:43 +0100

    modif de poussee etre valide

--- src/poussee.c
+++ src/poussee.c
@@ -30,9 +30,10 @@ void case_suivante_de_la_poussee(int* ptx,int* pty,orientation_deplacement orien
   *ptx=x;
 }
 
+
 int poussee_etre_valide(const plateau_siam* plateau,int x,int y,orientation_deplacement orientation)
 {
-  //on fait tous les assert du contrat
+   //on fait tous les assert du contrat
   assert(plateau!=NULL);
   assert(plateau_etre_integre(plateau));
   assert(coordonnees_etre_dans_plateau(x,y));
@@ -40,31 +41,41 @@ int poussee_etre_valide(const plateau_siam* plateau,int x,int y,orientation_depl
   assert(orientation_etre_integre(orientation));
   //on regarde quel orientation est opposee au mouvement
   orientation_deplacement orientationOpposeePoussee=orientation_inverser(orientation);
-  // on recupere la piece
-  const piece_siam* piece=plateau_obtenir_piece_info(plateau,x,y);
-  assert(piece!=NULL);
-  if(piece_etre_animal(piece)){
-    orientation_deplacement orientationPiece=piece_recuperer_orientation_animal(piece);
-    //si la piece est un animal oppose au mouvement alors il n'est pas possible
-    if(orientationPiece==orientationOpposeePoussee)
-      return 0;
-  }
-  //sinon on a une case non vide non opposée au mouvement mais peut-etre que la case suivante l'est
-  else
-  {
-    //on regarde quel est la case suivante
+  /*on initialise une variable qui nous permettera de voir le nombre d'animaux qui poussent
+    un ou plusieurs rochers*/
+   int nbrAnimauxMoinsRochers=1;
+  while(coordonnees_etre_dans_plateau(x,y)){
+    // on recupere la piece indiquee par les coordonnees
+    const piece_siam* piece=plateau_obtenir_piece_info(plateau,x,y);
+    assert(piece!=NULL);
+    if(piece_etre_animal(piece)){
+      orientation_deplacement orientationPiece=piece_recuperer_orientation_animal(piece);
+      //si la piece est un animal oppose au mouvement alors il n'est pas possible
+      if(orientationPiece==orientationOpposeePoussee)
+	return 0;
+      // sinon on regarde si son orientation s'ajoute a la poussee
+      else if(orientationPiece==orientation)
+	nbrAnimauxMoinsRochers++;
+    }
+    /*si on a un rocher alors on diminue nbrAnimauxMoinsRochers, et si il s'avere qu'il devient
+      negatif alors ca signifie qu'il y a plus de rochers que d'animaux qui poussent.
+      La poussee n'est donc pas possible*/
+   else if(piece_etre_rocher(piece)==1){
+      nbrAnimauxMoinsRochers--;
+      if(nbrAnimauxMoinsRochers<0)
+	return 0;
+    }
+    /*si on tombe sur une case vide alors on sort du while car il n'y a plus de pieces 
+      impliquees dans la poussee*/
+     else if(piece_etre_case_vide(piece)){
+      x=NBR_CASES+1;
+      y=NBR_CASES+1;
+    }
     case_suivante_de_la_poussee(&x,&y,orientation);
-    //si on est plus dans la plateau alors la poussee est possible
-    if(coordonnees_etre_dans_plateau(x,y)==0)
-      return 1;
-    //si la case est vide alors la poussee est possible
-    if(plateau_exister_piece(plateau,x,y)==0)
-      return 1;
-    //si on est pas dans les cas precedents alors on reapplique la fonction sur la case suivante
-    return poussee_etre_valide(plateau,x,y,orientation);
   }
-  puts("erreur de poussee_etre_valide");
-  exit(EXIT_FAILURE);
+  /*si on a atteint le return 1, cela signifie qu'on a pas rencontre d'animal oppose au mouvement
+    et que le nombre d'animaux qui poussent les rochers sont adequat. La poussee est donc possible*/
+  return 1;
 }
 
 void poussee_realiser(plateau_siam* plateau,int x,int y,orientation_deplacement orientation,
