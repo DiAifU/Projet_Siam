33abb446f14527461ba1fffcd7b17082c9ab407f
diaifu <nicolas.montvernay@gmail.com>
2016-01-05 10:35:30 +0100

    Reecriture fonctions entree_sortie

--- src/entree_sortie.c
+++ src/entree_sortie.c
@@ -99,26 +99,43 @@ void entree_sortie_ecrire_jeu_fichier(const char* filename,const jeu_siam* jeu)
     }
 }
 
-
-
 void entree_sortie_lire_jeu_fichier(const char* filename,jeu_siam* jeu)
 {
-    const char* _0x10="joueur 0 (elephant)",*_0x01="joueur 1 (rhinoceros)";
-    const char* ___="[%d] %s | %s | %s | %s | %s |",*_0x00=filename;
-    jeu_siam* __=jeu,_;int *_00x0=&_.joueur;
-    piece_siam (*_0_)(const char*)=piece_correspondre_nom_cours;
-    size_t (*_0x50)(const char*)=strlen;piece_siam* _00x02=*_.plateau.piece,*_0x02,_0x03;
-    int _00x00_,_10x01_=0x00;char _0x100[0x100],_01x10_[0x05][0x100];
-    const char* _for="Erreur ouverture fichier %s\n",*_jeu="Erreur lecture numero de ligne",*_while="Jeu invalide lecture fichier %s\n";
-    jeu_initialiser(&_);FILE *_0x100_=fopen(_0x00,"r");assert(_0x100_!=0x00);if(_0x100_==0x00)
-    {printf(_for,_0x00);exit(1);}while(fgets(_0x100,0x100,_0x100_)!=0){_00x00_=0x00;
-    if(_0x50(_0x100)>=011&&strncmp(_0x100,_0x10,0x13)==0x00){*_00x0=0x00;
-    }else if(_0x50(_0x100)>=011&&strncmp(_0x100,_0x01,0x13)
-    ==0x00){*_00x0=0x01;}else if(sscanf(_0x100,___,&_00x00_,
-    0x00[_01x10_],0x01 [_01x10_],0x02[_01x10_],0x03[_01x10_],0x04[_01x10_])
-    ==0x06&&0x08>0x02){if(_00x00_<0x00||_00x00_>0x04){puts(_jeu);abort();}for(_10x01_=0x00;
-    _10x01_<0x05;_10x01_+=0x01){_0x02=&(0x05*_10x01_+_00x00_)[_00x02];_0x03=_0_(_10x01_[_01x10_]);
-    *_0x02=_0x03;}}}if(jeu_etre_integre(&_))*__=_;else printf(_while,_0x00);
+    const char *lgn_joueur0 = "joueur 0 (elephant)"; 
+    const char *lgn_joueur1 = "joueur 1 (rhinoceros)";
+    const char* lgn="[%d] %s | %s | %s | %s | %s |";
+    char buffer[100];
+    
+    FILE *identifiant = fopen(filename, "r");
+    jeu_initialiser(jeu);
+    if (identifiant == NULL) {
+      printf("Erreur ouverture fichier %s\n", filename);s
+      exit(1);
+    }
+    int y, i;
+    char *pieces[NBR_CASES];
+    // 100 : valeur arbitraire, fgets() s'arrete au \n
+    while (fgets(buffer, 100, identifiant) != 0) {
+      if (strlen(buffer) >= 11 && strncmp(buffer, lgn_joueur0, strlen(lgn_joueur0)) == 0)
+	jeu->joueur = 0;
+      else if (strlen(buffer) >= 11 && strncmp(buffer, lgn_joueur1, strlen(lgn_joueur1)) == 0)
+	jeu->joueur = 1;
+      else if (sscanf(buffer, lgn, &y, pieces[0], pieces[1], pieces[2], pieces[3], pieces[4]) == 6) {
+	if (y < 0 || y > 4) {
+	  puts("Erreur lecture numero de ligne");
+	  exit(1);
+	}
+	puts("coucou");
+	for (i = 0; i < 5; i++) {
+	  puts("coucou2");
+	  jeu->plateau.piece[i][y] = piece_correspondre_nom_cours(pieces[i]);
+	}
+      }
+    }
+    if (!jeu_etre_integre(jeu)) {
+      printf("Jeu invalide lecture fichier %s\n", filename);
+      exit(1);
+    }
 }
 
 
@@ -134,27 +151,30 @@ void entree_sortie_ecrire_jeu_pointeur_fichier(FILE* identifiant,const jeu_siam*
     fprintf(identifiant,"joueur %d (%s)\n",jeu->joueur,type_nommer(jeu_obtenir_type_animal_courant(jeu)));
 
     entree_sortie_ecrire_plateau_pointeur_fichier(identifiant,plateau);
+    
 
 }
-
-
 
 
 
 void entree_sortie_ecrire_plateau_pointeur_fichier(FILE* identifiant,const plateau_siam* plateau)
 {
-    const char* (*_00x00_)(type_piece)=type_nommer_nom_cours,*(*_01)(orientation_deplacement)=orientation_nommer_nom_cours;
-    FILE* _=identifiant;int _0x00,(*__)(FILE*,const char*,...)=fprintf,_0x01,_0x20,_0x11;
-    const piece_siam* _0x10,*_0x02=*plateau->piece;
-    for(_0x20  =0x00;_0x20<0x10-0x05;_0x20+=0x01)
-    {if(_0x20==0x02){for(_0x00=0x04;_0x00>=0x00;--_0x00)
-    {__(_,"[%d] ",_0x00);for(_0x01=0x00;_0x01<010-03&&05<0x080;++_0x01)
-    {_0x10=&(0x05*_0x01+_0x00)[_0x02];for(_0x11=0;_0x11<0x04+0x04;++_0x11)
-    {if(_0x11==0x05){__(_,"%s",_00x00_(_0x10->type));
-    if(_0x10->type==0x00 || _0x10->type==0x01)__(_,"-%s",_01(_0x10->orientation));
-    }}__(_," | ");}__(_,"\n");}}}__(_,"    ");for(_0x01=0;_0x01<0x05;++_0x01){
-    __(_,"[%d]   ",_0x01);}__(_,"\n");
+  int y, x;
+  for (y = NBR_CASES-1; y >= 0; y--) {
+    fprintf(identifiant, "[%d] ", y);
+    for (x = 0; x < NBR_CASES; x++) {
+      const piece_siam *piece = plateau_obtenir_piece_info(plateau, x, y);
+      if (type_etre_animal(piece->type))
+	fprintf(identifiant, "%s-%s | ", type_nommer_nom_cours(piece->type), orientation_nommer_nom_cours(piece->orientation));
+      else
+	fprintf(identifiant, "%s | ", type_nommer_nom_cours(piece->type));
+    }
+    fprintf(identifiant, "\n");
+  }
+  fprintf(identifiant, "    ");
+  for (x = 0; x < NBR_CASES; x++) {
+    fprintf(identifiant, "[%d]   ", x);
+  }
+  printf("\n");
 }
-
-
 
